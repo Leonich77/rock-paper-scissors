2832290e9943d3127ef78c6b9743211ad79ed89d
Leonich77 <leonich777@rambler.ru>
2024-08-01 22:33:24 +0500

    Step 2: Write the logic to get the computer choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const MIN = 0;
+const MAX = 2;
+const stringValues = ["rock", "paper", "scissors"];
+
+const getComputerChoice = () => {
+  const randomValue = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+  return stringValues[randomValue];
+};
+
+console.log(getComputerChoice());
